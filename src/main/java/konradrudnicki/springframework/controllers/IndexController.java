af771e42ab534257c597e5d3466a0d730edf0140
konradrudnicki <konradrudnicki0@gmail.com>
2022-12-30 15:08:02 +0100

    Adding Lombok to the project

--- src/main/java/konradrudnicki/springframework/controllers/IndexController.java
+++ src/main/java/konradrudnicki/springframework/controllers/IndexController.java
@@ -6,12 +6,14 @@ import konradrudnicki.springframework.domain.UnitOfMeasure;
 import konradrudnicki.springframework.repositories.CategoryRepository;
 import konradrudnicki.springframework.repositories.UnitOfMeasureRepository;
 import konradrudnicki.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -23,6 +25,7 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting Index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
