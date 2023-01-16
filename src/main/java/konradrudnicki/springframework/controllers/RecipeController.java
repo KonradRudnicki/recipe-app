c686feb285833f0900d4f7a1bba0110a0aee505e
KonradRudnicki <97458824+KonradRudnicki@users.noreply.github.com>
2023-01-16 21:45:29 +0100

    Create a Recipe

--- src/main/java/konradrudnicki/springframework/controllers/RecipeController.java
+++ src/main/java/konradrudnicki/springframework/controllers/RecipeController.java
@@ -1,29 +1,40 @@
 package konradrudnicki.springframework.controllers;
 
+import konradrudnicki.springframework.commands.RecipeCommand;
 import konradrudnicki.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import static java.lang.Long.parseLong;
 
 @Controller
 public class RecipeController {
 
-    private final RecipeService recipeService ;
-
+    private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("recipe/show/{id}")
+    @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
 
-        model.addAttribute("recipe", recipeService.findById(parseLong(id)));
-
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
